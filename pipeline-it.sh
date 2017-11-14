eb3bd61983244867aa21997da00b0cd51be9d46e
NathanVanDalen <nathan_vandalen@hotmail.com>
2017-11-14 12:39:50 +0100

    Use docker-compose with image attr to find image

--- pipeline-it.sh
+++ pipeline-it.sh
@@ -5,8 +5,8 @@ set -ex
 source ./pipeline-it-exports.sh
 
 docker-compose -f docker-compose-it.yml down -v
-docker-compose -f docker-compose-it.yml pull
-docker-compose -f docker-compose-it.yml build
+#docker-compose -f docker-compose-it.yml pull
+#docker-compose -f docker-compose-it.yml build
 docker-compose -f docker-compose-it.yml up -d
 
 ./pipeline-it-setup.sh
